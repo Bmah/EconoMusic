ab39d0709238765e0cd42ea377bf82af6961ab02
Chris Ng <thecomediclife324@gmail.com>
2015-08-11 18:04:07 -0700

    Added Vibraphone to SoundLibrary script and prefab

--- EconoMusic/Assets/Scripts/SoundLibrary.cs
+++ EconoMusic/Assets/Scripts/SoundLibrary.cs
@@ -3,20 +3,38 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class SoundLibrary : MonoBehaviour {
+	//Enumeration of the various instruments available. Currently only Vibraphone has audio files.
 	public enum Instrument {vibraphone, politician, glaciar, mayonnaise, horseradish};
-	public Instrument instrument = Instrument.mayonnaise;
 
+	//Lists of each of the notes for vibraphone that gets placed in the nested List.
 	public List<AudioClip> vibraphoneE = new List<AudioClip>();
 	public List<AudioClip> vibraphoneH = new List<AudioClip>();
 	public List<AudioClip> vibraphoneQ = new List<AudioClip>();
-
-	// Use this for initialization
-	void Start () {
+	//Nested list for vibraphone that contains all of the notes.
+	public List<List<AudioClip>> vibraphone = new List<List<AudioClip>>();
 	
+	void Awake () {
+		//Fills the nested Array with its component parts.
+		for (int i = 0; i < 36; i++) {
+			vibraphone.Add (vibraphoneE);
+			vibraphone.Add (vibraphoneH);
+			vibraphone.Add (vibraphoneQ);
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	//This function returns the List of sound files you're looking for by inputting the instrument enum.
+	public List<List<AudioClip>> GetSoundFiles(Instrument instrumentName) {
+
+		switch (instrumentName) {
+
+		case Instrument.vibraphone:
+
+			return vibraphone;
+
+		default:
+
+			return vibraphone;
+		}
+
 	}
 }
