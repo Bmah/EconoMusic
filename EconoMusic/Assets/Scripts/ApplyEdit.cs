eba1eda53ed7aec10700d61468b21c02a892f5da
aajungroth <aajungroth@ucdavis.edu>
2015-08-21 13:49:06 -0700

    Editing Mode
    You can now reload a line you traced. There are some bugs with it still.

--- EconoMusic/Assets/Scripts/ApplyEdit.cs
+++ EconoMusic/Assets/Scripts/ApplyEdit.cs
@@ -6,8 +6,26 @@ using UnityEngine.UI;
 
 public class ApplyEdit : MonoBehaviour, IDropHandler{
 
-	//Holds wether or not this is an instrument (AAJ)
-	public bool isInstrument = false;
+	//Holds whether or not this is an instrument (AAJ)
+	private bool isInstrument = false;
+
+	//Holds the TracingManger so this script can get
+	//the components of the tracing scren (AAJ)
+	private GameObject tracingManager;
+
+	//Holds the tracing script so the graphs can be
+	//traced and edited (AAJ)
+	private TracingScript tracingScript;
+
+	//Holds the tracing screen (AAJ)
+	private GameObject tracingScreen;
+
+	//Holds the tracing image (AAJ)
+	private Image tracingGraph;
+
+	//Holds the DrawObject so the line can be
+	//traced or edited (AAJ)
+	private GameObject DrawObject;
 
 	// Use this for initialization
 	void Start ()
@@ -19,9 +37,18 @@ public class ApplyEdit : MonoBehaviour, IDropHandler{
 			//Sets this instance of ApplyEdit as an instrument (AAJ)
 			isInstrument = true;
 		}//if
+
+		//Finds the tracing manager (AAJ)
+		tracingManager = GameObject.FindGameObjectWithTag("Trace");
+
+		//Gets the components of the tracing screen (AAJ)
+		tracingScript = tracingManager.GetComponent<TracingScript>();
+		tracingScreen = tracingScript.tracingScreen;
+		tracingGraph = tracingScript.tracingGraph;
+
 	}//Start
 
-	//returns the first child (AAJ)
+	//Returns the first child (AAJ)
 	public GameObject item{
 		
 		get{
@@ -59,8 +86,9 @@ public class ApplyEdit : MonoBehaviour, IDropHandler{
 				if(DragAndDrop.itemBeingDragged.GetComponent<GraphReceiver>() == null){
 
 					if(isInstrument == false){
+
 						//Calls a function that will apply an edit to an instrument (AAJ)
-						//fooEditMode();
+						EditMode();
 					
 						//Test print
 						Debug.Log("Editing Mode");
@@ -72,4 +100,35 @@ public class ApplyEdit : MonoBehaviour, IDropHandler{
 		}//if
 	}//OnDrop
 	#endregion
-}
+
+	/// <summary>
+	/// This will allow the user to edit existing instruments (AAJ)
+	/// </summary>
+	void EditMode(){
+
+		//Enables the tracing screen(AAJ)
+		tracingScreen.SetActive(true);
+
+		//Gets the draw object (AAJ)
+		DrawObject = GameObject.FindGameObjectWithTag("Draw");
+
+		//Holds of chain of transforms that will be used to get
+		//the raw data in instrument script (AAJ)
+		Transform graphSlot = DragAndDrop.itemBeingDragged.transform.parent;
+		Transform graphPanel = graphSlot.transform.parent;
+		Transform instrumentObject = graphPanel.transform.parent;
+
+		//Updates the tracing screen with the editable line (AAJ)
+		DrawObject.GetComponent<DrawLine>().UpdateLine(instrumentObject.GetComponent<InstrumentScript>().RawData);
+
+		//Sets the draw object in tracing script (AAJ)
+		tracingScript.SetDrawObject(DrawObject);
+
+		//Copies the instruments sprite to the tracing screen (AAJ)
+		tracingGraph.sprite = GetComponent<Image>().sprite;
+
+		//Passes the name of the image to the tracing script(AAJ)
+		//Instrument scirpt needs to updated to hold the image's name (AAJ)
+		//tracingScript.SetFileName();
+	}//EditMode
+}//ApplyEdit
