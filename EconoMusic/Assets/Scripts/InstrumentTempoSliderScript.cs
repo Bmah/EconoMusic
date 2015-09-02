571354310cd76718aaffbdae2c6e20f334c86664
Brian M <bkmah@ucdavis.edu>
2015-09-02 16:53:44 -0700

    Altered Instruments to fix bugs
    Got rid of apply button
    Added script to take care of notes on tempo slider
    added script to handle highlighted colors on play pause and loop
    also made scrubbing work properly

---
+++ EconoMusic/Assets/Scripts/InstrumentTempoSliderScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class InstrumentTempoSliderScript : MonoBehaviour {
+
+	private Image note;
+	public Slider tempoSlider;
+	public Sprite eigthNote, quarterNote, halfNote;
+
+	// Use this for initialization
+	void Start () {
+		note = this.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(tempoSlider.value < 0.5f){
+			note.sprite = eigthNote;
+		}//if
+		else if(tempoSlider.value < 1f){
+			note.sprite = quarterNote;
+		}//else if
+		else{
+			note.sprite = halfNote;
+		}//else
+
+	}
+}
